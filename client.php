6f7d7f2e64606e2d5518b368639bf6e276631149
songshu <2723659854@qq.com>
2025-01-06 15:28:41 +0800

    使用fiber构建的服务端

--- client.php
+++ client.php
@@ -6,19 +6,21 @@ if ($clientSocket === false) {
     die('无法创建客户端套接字: '. socket_strerror(socket_last_error()));
 }
 
+
 // 连接到服务器
 $serverAddress = '127.0.0.1';
 $serverPort = 8888;
 if (socket_connect($clientSocket, $serverAddress, $serverPort) === false) {
     die('无法连接到服务器: '. socket_strerror(socket_last_error()));
 }
+var_dump("已连接服务器");
 
 // 发送数据到服务器
 $message = "你好，服务端";
 if (socket_write($clientSocket, $message) === false) {
     die('无法发送消息到服务器: '. socket_strerror(socket_last_error()));
 }
-
+var_dump("发送消息完成");
 // 接收服务器返回的响应
 $response = socket_read($clientSocket, 1024);
 if ($response === false) {
---
+++ server.php
@@ -0,0 +1,56 @@
+<?php
+/** 以下是使用fiber协程创建的服务端 */
+// 引入自动加载文件（假设通过Composer管理依赖）
+require 'vendor/autoload.php';
+// 引入Revolt\EventLoop命名空间
+use Revolt\EventLoop;
+
+// 创建服务器套接字资源，基于TCP协议监听在本地127.0.0.1的8888端口
+$serverSocket = stream_socket_server("tcp://127.0.0.1:8888", $errno, $errstr);
+if (!$serverSocket) {
+    die("无法创建服务器套接字: $errstr ($errno)");
+}
+echo "=================================================\r\n";
+
+// 当有新的客户端连接时的回调函数
+$onConnect = function ($clientSocket) {
+    echo "客户端发起连接\r\n";
+    // 当从客户端接收到数据时的回调函数
+    $onData = function () use($clientSocket){
+        echo "接收到客户端数据\r\n";
+        $data = fread($clientSocket, 1024);
+        if ($data === false) {
+            var_dump("未接受到数据，关闭1");
+            // 如果读取数据出错，关闭客户端连接
+            fclose($clientSocket);
+            return;
+        }
+        if ($data === "") {
+            var_dump("接受的数据为空，关闭2");
+            // 如果客户端关闭连接，也关闭对应的客户端套接字
+            fclose($clientSocket);
+            return;
+        }
+        var_dump($data);
+        // 处理客户端数据，这里简单回显
+        $response = "收到你的消息: " . $data;
+        fwrite($clientSocket, $response);
+    };
+
+    // 为客户端套接字添加可读事件监听器，当有数据可读时触发$onData回调
+    EventLoop::onReadable($clientSocket, $onData);
+};
+
+// 为服务器套接字添加可读事件监听器，当有新客户端连接时触发$onConnect回调
+EventLoop::onReadable($serverSocket, function () use ($serverSocket, $onConnect) {
+    $clientSocket = stream_socket_accept($serverSocket);
+    if ($clientSocket) {
+        // 调用 $onConnect 回调函数处理新连接
+        $onConnect($clientSocket);
+    }
+});
+
+// 启动事件循环，驱动协程运行，处理各种事件
+EventLoop::run();
+
+?>
