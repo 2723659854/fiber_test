653dd7f412130e26b5db027eb6f9a3a9220b4b0a
songshu <2723659854@qq.com>
2025-01-07 15:28:26 +0800

    注释

--- vendor/revolt/event-loop/src/EventLoop/Driver/TracingDriver.php
+++ vendor/revolt/event-loop/src/EventLoop/Driver/TracingDriver.php
@@ -9,6 +9,9 @@ use Revolt\EventLoop\Driver;
 use Revolt\EventLoop\InvalidCallbackError;
 use Revolt\EventLoop\Suspension;
 
+/**
+ * @purpose 追踪驱动
+ */
 final class TracingDriver implements Driver
 {
     private readonly Driver $driver;
@@ -25,44 +28,75 @@ final class TracingDriver implements Driver
     /** @var array<string, string> */
     private array $cancelTraces = [];
 
+    /**
+     * 初始化
+     * @param Driver $driver
+     */
     public function __construct(Driver $driver)
     {
         $this->driver = $driver;
     }
 
+    /**
+     * 启动
+     * @return void
+     */
     public function run(): void
     {
         $this->driver->run();
     }
 
+    /**
+     * 停止
+     * @return void
+     */
     public function stop(): void
     {
         $this->driver->stop();
     }
 
+    /**
+     * 挂起
+     * @return Suspension
+     */
     public function getSuspension(): Suspension
     {
         return $this->driver->getSuspension();
     }
 
+    /**
+     * 判断是否在运行
+     * @return bool
+     */
     public function isRunning(): bool
     {
         return $this->driver->isRunning();
     }
 
+    /**
+     * 延迟执行
+     * @param \Closure $closure
+     * @return string
+     */
     public function defer(\Closure $closure): string
     {
         $id = $this->driver->defer(function (...$args) use ($closure) {
             $this->cancel($args[0]);
             return $closure(...$args);
         });
-
+        /** 记录追踪日志 */
         $this->creationTraces[$id] = $this->formatStacktrace(\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS));
         $this->enabledCallbacks[$id] = true;
 
         return $id;
     }
 
+    /**
+     * 延迟执行
+     * @param float $delay
+     * @param \Closure $closure
+     * @return string
+     */
     public function delay(float $delay, \Closure $closure): string
     {
         $id = $this->driver->delay($delay, function (...$args) use ($closure) {
@@ -76,6 +110,12 @@ final class TracingDriver implements Driver
         return $id;
     }
 
+    /**
+     * 重复执行
+     * @param float $interval
+     * @param \Closure $closure
+     * @return string
+     */
     public function repeat(float $interval, \Closure $closure): string
     {
         $id = $this->driver->repeat($interval, $closure);
@@ -86,6 +126,12 @@ final class TracingDriver implements Driver
         return $id;
     }
 
+    /**
+     * 可读事件
+     * @param mixed $stream
+     * @param \Closure $closure
+     * @return string
+     */
     public function onReadable(mixed $stream, \Closure $closure): string
     {
         $id = $this->driver->onReadable($stream, $closure);
@@ -96,6 +142,12 @@ final class TracingDriver implements Driver
         return $id;
     }
 
+    /**
+     * 可写事件
+     * @param mixed $stream
+     * @param \Closure $closure
+     * @return string
+     */
     public function onWritable(mixed $stream, \Closure $closure): string
     {
         $id = $this->driver->onWritable($stream, $closure);
@@ -106,6 +158,13 @@ final class TracingDriver implements Driver
         return $id;
     }
 
+    /**
+     * 信号处理
+     * @param int $signal
+     * @param \Closure $closure
+     * @return string
+     * @throws \Revolt\EventLoop\UnsupportedFeatureException
+     */
     public function onSignal(int $signal, \Closure $closure): string
     {
         $id = $this->driver->onSignal($signal, $closure);
@@ -116,6 +175,11 @@ final class TracingDriver implements Driver
         return $id;
     }
 
+    /**
+     * 开启追踪
+     * @param string $callbackId
+     * @return string
+     */
     public function enable(string $callbackId): string
     {
         try {
@@ -131,6 +195,11 @@ final class TracingDriver implements Driver
         return $callbackId;
     }
 
+    /**
+     * 取消追踪
+     * @param string $callbackId
+     * @return void
+     */
     public function cancel(string $callbackId): void
     {
         $this->driver->cancel($callbackId);
@@ -142,6 +211,11 @@ final class TracingDriver implements Driver
         unset($this->enabledCallbacks[$callbackId], $this->unreferencedCallbacks[$callbackId]);
     }
 
+    /**
+     * 关闭追踪
+     * @param string $callbackId
+     * @return string
+     */
     public function disable(string $callbackId): string
     {
         $this->driver->disable($callbackId);
@@ -150,6 +224,11 @@ final class TracingDriver implements Driver
         return $callbackId;
     }
 
+    /**
+     * 引用
+     * @param string $callbackId
+     * @return string
+     */
     public function reference(string $callbackId): string
     {
         try {
@@ -165,6 +244,11 @@ final class TracingDriver implements Driver
         return $callbackId;
     }
 
+    /**
+     * 取消引用
+     * @param string $callbackId
+     * @return string
+     */
     public function unreference(string $callbackId): string
     {
         $this->driver->unreference($callbackId);
@@ -173,11 +257,20 @@ final class TracingDriver implements Driver
         return $callbackId;
     }
 
+    /**
+     * 设定错误处理器
+     * @param \Closure|null $errorHandler
+     * @return void
+     */
     public function setErrorHandler(?\Closure $errorHandler): void
     {
         $this->driver->setErrorHandler($errorHandler);
     }
 
+    /**
+     * 获取错误处理器
+     * @return \Closure|null
+     */
     public function getErrorHandler(): ?\Closure
     {
         return $this->driver->getErrorHandler();
@@ -189,6 +282,10 @@ final class TracingDriver implements Driver
         return $this->driver->getHandle();
     }
 
+    /**
+     * 打印追踪记录
+     * @return string
+     */
     public function dump(): string
     {
         $dump = "Enabled, referenced callbacks keeping the loop running: ";
@@ -206,47 +303,86 @@ final class TracingDriver implements Driver
         return \rtrim($dump);
     }
 
+    /** 获取识别符号
+     * @return array|string[]
+     */
     public function getIdentifiers(): array
     {
         return $this->driver->getIdentifiers();
     }
 
+    /**
+     * 获取回调类型
+     * @param string $callbackId
+     * @return CallbackType
+     */
     public function getType(string $callbackId): CallbackType
     {
         return $this->driver->getType($callbackId);
     }
 
+    /**
+     * 是否可用
+     * @param string $callbackId
+     * @return bool
+     */
     public function isEnabled(string $callbackId): bool
     {
         return $this->driver->isEnabled($callbackId);
     }
 
+    /**
+     * 是否可引用
+     * @param string $callbackId
+     * @return bool
+     */
     public function isReferenced(string $callbackId): bool
     {
         return $this->driver->isReferenced($callbackId);
     }
 
+    /**
+     * 调试
+     * @return array
+     */
     public function __debugInfo(): array
     {
         return $this->driver->__debugInfo();
     }
 
+    /**
+     * 投递到队列
+     * @param \Closure $closure
+     * @param mixed ...$args
+     * @return void
+     */
     public function queue(\Closure $closure, mixed ...$args): void
     {
         $this->driver->queue($closure, ...$args);
     }
 
+    /**
+     * 获取创建追踪信息
+     * @param string $callbackId
+     * @return string
+     */
     private function getCreationTrace(string $callbackId): string
     {
         return $this->creationTraces[$callbackId] ?? 'No creation trace, yet.';
     }
 
+    /**
+     * 获取取消追踪信息
+     * @param string $callbackId
+     * @return string
+     */
     private function getCancelTrace(string $callbackId): string
     {
         return $this->cancelTraces[$callbackId] ?? 'No cancellation trace, yet.';
     }
 
     /**
+     * 格式化追踪信息
      * Formats a stacktrace obtained via `debug_backtrace()`.
      *
      * @param list<array{
--- vendor/revolt/event-loop/src/EventLoop/Driver/UvDriver.php
+++ vendor/revolt/event-loop/src/EventLoop/Driver/UvDriver.php
@@ -12,8 +12,13 @@ use Revolt\EventLoop\Internal\StreamReadableCallback;
 use Revolt\EventLoop\Internal\StreamWritableCallback;
 use Revolt\EventLoop\Internal\TimerCallback;
 
+/**
+ * @purpose uv驱动
+ * @note 代码风格：上面写静态方法，下面写动态方法
+ */
 final class UvDriver extends AbstractDriver
 {
+    /** 是否支持uv扩展 */
     public static function isSupported(): bool
     {
         return \extension_loaded("uv");
@@ -34,12 +39,14 @@ final class UvDriver extends AbstractDriver
     public function __construct()
     {
         parent::__construct();
-
+        /** 调用libuv扩展，是C库 ，分配内存，并返回一个事件 */
         $this->handle = \uv_loop_new();
-
+        /** 读写事件 */
         $this->ioCallback = function ($event, $status, $events, $resource): void {
+            /** 取出回调函数 */
             $callbacks = $this->uvCallbacks[(int) $event];
-
+            //调用错误回调，因为这将行为与其他循环后端相匹配。
+            //重新启用回调，因为libuv在非零状态下禁用回调。
             // Invoke the callback on errors, as this matches behavior with other loop back-ends.
             // Re-enable callback as libuv disables the callback on non-zero status.
             if ($status !== 0) {
@@ -49,22 +56,24 @@ final class UvDriver extends AbstractDriver
 
                     $flags |= $callback->invokable ? $this->getStreamCallbackFlags($callback) : 0;
                 }
+                /** 对一个io链接启动poll轮训 参数：文件描述符即连接 ，读写描述符，回调函数 */
                 \uv_poll_start($event, $flags, $this->ioCallback);
             }
 
+            /** 遍历所有的回调函数 */
             foreach ($callbacks as $callback) {
                 \assert($callback instanceof StreamCallback);
-
+                /** 获取所有事件的状态 */
                 // $events is ORed with 4 to trigger callback if no events are indicated (0) or on UV_DISCONNECT (4).
                 // http://docs.libuv.org/en/v1.x/poll.html
                 if (!($this->getStreamCallbackFlags($callback) & $events || ($events | 4) === 4)) {
                     continue;
                 }
-
+                /** 将回调函数投递到队列中 */
                 $this->enqueueCallback($callback);
             }
         };
-
+        /** 定时任务 */
         $this->timerCallback = function ($event): void {
             $callback = $this->uvCallbacks[(int) $event][0];
 
@@ -72,7 +81,7 @@ final class UvDriver extends AbstractDriver
 
             $this->enqueueCallback($callback);
         };
-
+        /** 信号处理器 */
         $this->signalCallback = function ($event): void {
             $callback = $this->uvCallbacks[(int) $event][0];
 
@@ -81,6 +90,7 @@ final class UvDriver extends AbstractDriver
     }
 
     /**
+     * 取消回调函数
      * {@inheritdoc}
      */
     public function cancel(string $callbackId): void
@@ -127,29 +137,32 @@ final class UvDriver extends AbstractDriver
     }
 
     /**
+     * 启动事件
      * {@inheritdoc}
      */
     protected function dispatch(bool $blocking): void
     {
+        /** 可以只执行一次，可以持续执行 */
         /** @psalm-suppress TooManyArguments */
         \uv_run($this->handle, $blocking ? \UV::RUN_ONCE : \UV::RUN_NOWAIT);
     }
 
     /**
+     * 激活事件
      * {@inheritdoc}
      */
     protected function activate(array $callbacks): void
     {
         $now = $this->now();
-
+        /** 遍历所有事件 */
         foreach ($callbacks as $callback) {
             $id = $callback->id;
-
+            /** 如果是读写流 */
             if ($callback instanceof StreamCallback) {
                 \assert(\is_resource($callback->stream));
 
                 $streamId = (int) $callback->stream;
-
+                /** 取出文件描述符，就是io链接 */
                 if (isset($this->streams[$streamId])) {
                     $event = $this->streams[$streamId];
                 } elseif (isset($this->events[$id])) {
@@ -162,15 +175,17 @@ final class UvDriver extends AbstractDriver
                 $eventId = (int) $event;
                 $this->events[$id] = $event;
                 $this->uvCallbacks[$eventId][$id] = $callback;
-
+                /** 取出读写标识 */
                 $flags = 0;
                 foreach ($this->uvCallbacks[$eventId] as $w) {
                     \assert($w instanceof StreamCallback);
 
                     $flags |= $w->enabled ? ($this->getStreamCallbackFlags($w)) : 0;
                 }
+                /** 启动这个链接的io读写事件 */
                 \uv_poll_start($event, $flags, $this->ioCallback);
             } elseif ($callback instanceof TimerCallback) {
+                /** 获取定时任务 */
                 if (isset($this->events[$id])) {
                     $event = $this->events[$id];
                 } else {
@@ -178,7 +193,7 @@ final class UvDriver extends AbstractDriver
                 }
 
                 $this->uvCallbacks[(int) $event] = [$callback];
-
+                /** 启动定时任务 */
                 \uv_timer_start(
                     $event,
                     (int) \min(\max(0, \ceil(($callback->expiration - $now) * 1000)), \PHP_INT_MAX),
@@ -195,6 +210,7 @@ final class UvDriver extends AbstractDriver
 
                 $this->uvCallbacks[(int) $event] = [$callback];
 
+                /** 启动信号处理 */
                 /** @psalm-suppress TooManyArguments */
                 \uv_signal_start($event, $this->signalCallback, $callback->signal);
             } else {
@@ -206,6 +222,7 @@ final class UvDriver extends AbstractDriver
     }
 
     /**
+     * 挂起事件
      * {@inheritdoc}
      */
     protected function deactivate(DriverCallback $callback): void
@@ -246,13 +263,20 @@ final class UvDriver extends AbstractDriver
         }
     }
 
+    /**
+     * 判断回调事件类型
+     * @param StreamCallback $callback
+     * @return int
+     */
     private function getStreamCallbackFlags(StreamCallback $callback): int
     {
         if ($callback instanceof StreamWritableCallback) {
+            /** 可写2 */
             return \UV::WRITABLE;
         }
 
         if ($callback instanceof StreamReadableCallback) {
+            /** 可读1 */
             return \UV::READABLE;
         }
 
